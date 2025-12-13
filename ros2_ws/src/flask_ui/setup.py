c1f90c31225747b70e5be1f063e5349e6d19d2a5
joshlinx69 <lyou09134@gmail.com>
2025-12-13 22:46:29 +0800

    dick

--- ros2_ws/src/flask_ui/setup.py
+++ ros2_ws/src/flask_ui/setup.py
@@ -1,34 +1,32 @@
 from setuptools import find_packages, setup
-import os
 from glob import glob
+import os
 
 package_name = 'flask_ui'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/flask_ui"]),
-        ("share/flask_ui", ["package.xml"]),
+        ("share/ament_index/resource_index/packages", 
+            ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
         
-        (os.path.join("share", "flask_ui", "templates"),
-        glob("flask_ui/templates/*")),
-        
-        (os.path.join("share", "flask_ui", "static"),
-        glob("flask_ui/static/**/*", recursive=True)),
+        (os.path.join("share", package_name, "static"),
+         glob("flask_ui/static/**/*", recursive=True)),
+
+        (os.path.join("share", package_name, "templates"),
+         glob("flask_ui/templates/*")),
     ],
     
     install_requires=['setuptools', 'flask'],
     zip_safe=True,
     maintainer='joshlin69',
-    maintainer_email='lyou09134@gmail.com',
-    description='Flask web UI integraged with ROS2',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    description='Flask web UI for ROS 2 robot system',
     entry_points={
         'console_scripts': [
-            "flask_node = flask_ui.node:main",
+            'flask_node = flask_ui.node:main',
         ],
     },
 )
